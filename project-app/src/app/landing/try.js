1c6805ea893ff703def32e327d8688dad642db36
harshj4 <47681887+harshj4@users.noreply.github.com>
2019-04-09 05:43:58 -0400

    Simple login functionality added.

--- project-app/src/app/landing/try.js
+++ project-app/src/app/landing/try.js
@@ -1,16 +1,26 @@
-//var greet = require('./greetings'); // Local Module
 var express = require('express'); // Third Party Module
 var spawn = require("child_process").spawn; 
+var path = require('path');
 var app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(express.static(__dirname))
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+
 app.get('/', function (req, res) {
-    res.sendFile('landing.component.html',{root:__dirname});
+    res.sendFile(path.resolve('../login/login.html'));
 });
 
 app.post('/login', function (req, res) {
-    res.send("ved");
+    user = req.param('user');
+    pass = req.param('pass');
+    if(pass == "timepass"){
+        res.redirect('/landing');
+    }
+    else{
+        res.send('Auth failed');
+    }
 });
 
 app.get('/keywords', function(req, res){
@@ -22,5 +32,9 @@ app.get('/keywords', function(req, res){
         res.send(data); 
     } ) 
     //res.send(resp);
+})
+
+app.get('/landing',function(req,res){
+    res.sendFile('landing.component.html',{root:__dirname});
 })
 app.listen(3000);
