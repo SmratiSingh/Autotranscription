aef31abfd3ed0c3cf9889598d2d2ae829c1ea6c8
ved <ved28592@gmail.com>
2019-04-26 02:49:48 -0400

    changed url for node as well

--- project-app/src/app/landing/try.js
+++ project-app/src/app/landing/try.js
@@ -23,7 +23,7 @@ app.get('/', function (req, res) {
 });
 
 app.use(function (req, res, next) {        
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');    
+    res.setHeader('Access-Control-Allow-Origin', 'http://stark.cse.buffalo.edu:4200');    
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');      
     res.setHeader('Access-Control-Allow-Credentials', true);       
