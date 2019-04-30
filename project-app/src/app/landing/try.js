c5418ff26e18114571ff61e09e0dbb33a407e0d9
harshj4 <47681887+harshj4@users.noreply.github.com>
2019-04-30 11:58:43 -0400

    Side nav integrated.

--- project-app/src/app/landing/try.js
+++ project-app/src/app/landing/try.js
@@ -40,7 +40,7 @@ app.post('/login', function (req, res) {
     mongo.verifyLogin(user, pass, function(document){
         console.log(document);
         console.log("authenticating");
-        if(document == null){res.send('FAIL');}
+        if(document == null || length(document)==0){res.send('FAIL');}
         else if(pass == document.password) {
             console.log("Login successful for :" + document.username);
 
