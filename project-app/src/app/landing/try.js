cd1c77093492bda7dc9e990191322fde8f756710
harshj4 <47681887+harshj4@users.noreply.github.com>
2019-05-03 07:30:09 -0400

    working with node

--- project-app/src/app/landing/try.js
+++ project-app/src/app/landing/try.js
@@ -150,7 +150,7 @@ app.get('/keywords', function(req, res){
 });
 
 app.post('/transcript', function(req, res){
-    console.log('save data is: '+ req.body);
+    console.log('save data is: '+ req.body.username);
     username = req.body.username;
     trans_text = req.body.text;
     keywords = req.body.keywords;
