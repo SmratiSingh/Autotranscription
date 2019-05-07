47704c85526e32f41e697053b0f6e7998453d44c
harshj4 <47681887+harshj4@users.noreply.github.com>
2019-05-07 07:32:55 -0400

    Bubble backend and server calls done.

--- project-app/src/app/auth.service.ts
+++ project-app/src/app/auth.service.ts
@@ -30,7 +30,7 @@ export class AuthService {
     sessionStorage.setItem('isLoggedIn','true');
     sessionStorage.setItem('username',user.username);
     // return this.http.post('https://localhost:3000/signup', user).map((response: Response) =>response.json())              
-    return this.http.post('http://stark.cse.buffalo.edu:3000/signup', user).map((response: Response) =>response.json())              
+    return this.http.post('http://stark.cse.buffalo.edu:3000/signup', user).map((response: Response) =>response.json());              
   }
 
 }
